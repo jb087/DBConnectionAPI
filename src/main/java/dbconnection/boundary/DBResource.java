c99e9de8c810383c3fe9381aa972d72e9987a5bd
jb087 <jb087@op.pl>
2019-08-30 11:35:16 +0200

    # Added endpoint to get data by sensor id and sort by date # Added endpoint to generate data

--- src/main/java/dbconnection/boundary/DBResource.java
+++ src/main/java/dbconnection/boundary/DBResource.java
@@ -5,22 +5,43 @@ import dbconnection.entity.Temperature;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
 
-@Path("db")
+@Path(DBResource.DB_PATH)
 @Produces(MediaType.APPLICATION_JSON)
 public class DBResource {
 
+    public static final String DB_PATH = "db";
+    private static final String ALL_PATH = "all";
+    private static final String SENSOR_MEASUREMENT_PATH = "sensor/{id}";
+    private static final String GENERATE_MEASUREMENTS = "generate-measurements";
     @Inject
     private DBManager dbManager;
 
     @GET
+    @Path(ALL_PATH)
     public Response selectAll() {
-        List<Temperature> temperatures = dbManager.getAllTemperatures();
+        List<Temperature> temperatures = dbManager.getAllMeasurement();
         return Response.ok(temperatures).header("Access-Control-Allow-Origin", "*").build();
+    }
+
+    @GET
+    @Path(SENSOR_MEASUREMENT_PATH)
+    public Response getSensorMeasurement(@PathParam("id") String sensorId) {
+        List<Temperature> temperatures = dbManager.getMeasurementForSensor(sensorId);
+        return Response.ok(temperatures).header("Access-Control-Allow-Origin", "*").build();
+    }
+
+    @POST
+    @Path(GENERATE_MEASUREMENTS)
+    public Response generateMeasurements() {
+        dbManager.generateTemperatures();
+        return Response.ok().header("Access-Control-Allow-Origin", "*").build();
     }
 }
--- src/main/java/dbconnection/control/DBManager.java
+++ src/main/java/dbconnection/control/DBManager.java
@@ -9,7 +9,11 @@ import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Random;
+import java.util.UUID;
 
 @Stateless
 public class DBManager {
@@ -17,7 +21,7 @@ public class DBManager {
     @PersistenceContext(unitName = "myPU")
     private EntityManager entityManager;
 
-    public List<Temperature> getAllTemperatures() {
+    public List<Temperature> getAllMeasurement() {
         //return entityManager.createQuery("SELECT t FROM Temperature t").getResultList();
 
         CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
@@ -27,5 +31,37 @@ public class DBManager {
         criteriaQuery.select(root);
         TypedQuery<Temperature> typedQuery = entityManager.createQuery(criteriaQuery);
         return typedQuery.getResultList();
+    }
+
+    public List<Temperature> getMeasurementForSensor(String sensorId) {
+        return entityManager.createQuery("SELECT t FROM Temperature t WHERE t.sensorId =" + sensorId + " ORDER BY t.datetime").getResultList();
+    }
+
+    public void saveMeasurement(Temperature temperature) {
+        entityManager.persist(temperature);
+    }
+
+    public void generateTemperatures() {
+        List<Temperature> temperatures = new ArrayList<>();
+
+        for (int i = 0; i < 100; i++) {
+            Temperature temperature = new Temperature();
+            temperature.setId(UUID.randomUUID().hashCode());
+            temperature.setSensorId(6);
+
+            Random random = new Random();
+            temperature.setTemperature(36.0f + random.nextFloat());
+
+            temperature.setDatetime(new Date());
+
+            temperatures.add(temperature);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        temperatures.forEach(this::saveMeasurement);
     }
 }
